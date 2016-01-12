61210aa9e2a82a34a61246b22b5470ef625756bf
tangshijun <tangshijun@qianmi.com>
2016-01-12 11:22:30 +0800

    Merge branch 'master' of https://github.com/QMopenteam/sdk-demo
    # Conflicts:
    #	sdk-demo.iml

--- src/main/java/com/qianmi/open/sdk/web/controller/recharge/MobileController.java
+++ src/main/java/com/qianmi/open/sdk/web/controller/recharge/MobileController.java
@@ -5,41 +5,42 @@ import com.qianmi.open.api.DefaultOpenClient;
 import com.qianmi.open.api.OpenClient;
 import com.qianmi.open.api.request.RechargeMobileCreateBillRequest;
 import com.qianmi.open.api.request.RechargeMobileGetItemInfoRequest;
-import com.qianmi.open.api.request.RechargeMobileGetItemListRequest;
 import com.qianmi.open.api.response.RechargeMobileCreateBillResponse;
 import com.qianmi.open.api.response.RechargeMobileGetItemInfoResponse;
-import com.qianmi.open.api.response.RechargeMobileGetItemListResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
- * 话费充值
+ * 话费充值sdkdemo
+ *
+ * 说明:话费充值根据面值选择商品有2种方式,1,自动选择最优商品(规则参阅下方),2获得所有支持商品的列表,
+ * '本demo使用的是,方式1'
+ *
+ * PS:下单成功，请在30分钟内调用'支付接口{qianmi.elife.recharge.base.payBill}',完成支付，否则必须重新下单。
+ * 本demo不含支付.
+ .
  * Created by qmopen on 16/1/9.
  */
 @Controller
 @RequestMapping("/mobile")
 public class MobileController {
 
-    private static final String url = "url";
+    private static final String url = "gw.api.qianmi.com/api";
     private static final String appKey = "{你的APPKEY}";
     private static final String appSecret = "{你的appSecret}";
     private static final String accessToken = "{你的accessToken}";
 
-
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
-        model.addAttribute("message", "Hello world!");
         return "mobile-recharge";
     }
 
 
     /**
+     * 方式1
      * 第一步,根据手机号码加上面值取得支持该号码的商品
      * 1.返回指定面值，手机号所在区域下优先级最高商品，优先级："市>省>全国，固定面值>任意充"
      * 2.在同样充值金额下，满足客户选择不同商品需求，可选择与 "查询话费直充商品列表"接口分场景使用
@@ -58,19 +59,24 @@ public class MobileController {
         return response;
     }
 
-//    /**
-//     * 第二步,根据选择的面值获取商品详情
-//     *
-//     * @throws ApiException
-//     */
-//
-//    public void mobileGetItemInfo() throws ApiException {
-//        OpenClient client = new DefaultOpenClient(url, appKey, appSecret);
-//        RechargeMobileGetItemInfoRequest req = new RechargeMobileGetItemInfoRequest();
-//        req.setMobileNo("13333333333");
-//        req.setRechargeAmount("100");
-//        RechargeMobileGetItemInfoResponse response = client.execute(req, accessToken);
-//    }
+    /**
+     * 方式2
+     * 第一步,根据手机号码加上面值取得支持该号码的商品列表
+     * 1.返回指定面值下手机号所在区域商品列表,包含任意充商品；
+     * 2.在同样充值金额下，满足客户选择不同商品需求，可选择与 "查询单个话费充值商品"接口分场景使用。
+     * 3.可放弃此步骤，直接根据手机号码、充值金额直接生成订单(知道商品编号的前提下)。
+     *
+     * @throws ApiException
+     */
+    @RequestMapping(value = "/itemList")
+    public void mobileGetItemInfo() throws ApiException {
+        OpenClient client = new DefaultOpenClient(url, appKey, appSecret);
+        RechargeMobileGetItemInfoRequest req = new RechargeMobileGetItemInfoRequest();
+        req.setMobileNo("13333333333");
+        req.setRechargeAmount("100");
+        RechargeMobileGetItemInfoResponse response = client.execute(req, accessToken);
+    }
+
 
     /**
      * 第二步,调用下单接口
@@ -89,13 +95,4 @@ public class MobileController {
         return  response;
     }
 
-    public static void main(String[] args) {
-        try {
-            String mobileNo = "13770786502";
-            String rechargeAmout = "50";
-            new MobileController().mobileGetItemInfo(mobileNo,rechargeAmout);
-        } catch (ApiException e) {
-            e.printStackTrace();
-        }
-    }
 }
