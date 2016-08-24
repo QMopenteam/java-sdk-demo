c2d8751229e494e121149902e7c6957fa6218757
zhangcm <justif_z@163.com>
2016-08-24 15:19:07 +0800

    去除配置文件中的空格

--- src/main/java/com/qianmi/open/sdk/web/controller/BaseController.java
+++ src/main/java/com/qianmi/open/sdk/web/controller/BaseController.java
@@ -11,10 +11,10 @@ public class BaseController {
 
     private static final String API_URL = "http://gw.api.qianmi.com/api";
 
-    protected static final String APP_KEY = Constants.getProperty("appKey");
-    protected static final String APP_SECRET = Constants.getProperty("appSecret");
+    protected static final String APP_KEY = Constants.getProperty("appKey").trim();
+    protected static final String APP_SECRET = Constants.getProperty("appSecret").trim();
 
-    protected static String accessToken = Constants.getProperty("accessToken");
+    protected static String accessToken = Constants.getProperty("accessToken").trim();
     // 创建一个客户端，默认连接超时时间为3秒，请求超时时间为15秒。
     protected static final OpenClient client = new DefaultOpenClient(API_URL, APP_KEY, APP_SECRET);
 
